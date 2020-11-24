c391bf146cd19428384ab45708e03551d9ea8e01
magnorod <marcboursetmarcellas@gmail.com>
2020-11-24 22:39:59 +0100

    Create tp1-script-centos.sh

---
+++ tp1-script-centos.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+touch resultat.txt
+
+adresse_ip=$(ip addr show dev eth0 | grep eth0 | grep inet | awk '{print $2}')
+espace_disque_dispo=$(df -h | awk '{print $4}')
+cpu=$(top -n1 -b | awk '{print $9}')
+
+resultat="
+adresse ip :
+$adresse_ip
+
+espace disque dispo:
+$espace_disque_dispo
+
+cpu:
+$cpu
+"
+echo "$resultat">>resultat-script.txt
+
+scp -r -p resultat-script.txt superv@172.18.10.20:/tmp/resultat-script-centos.txt
+
+rm  resultat-script.txt
